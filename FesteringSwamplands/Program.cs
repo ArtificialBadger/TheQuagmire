f4132063183769d3c4ecb0cacee4b639a56ad3ce
Isaac Zais <isaaczais@hotmail.com>
2022-05-19 22:53:28 -0700

    Added a wrapping CascadingAuthenticationState

--- FesteringSwamplands/Program.cs
+++ FesteringSwamplands/Program.cs
@@ -19,6 +19,13 @@ builder.Services.AddHttpClient();
 
 //builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    // Configure your authentication provider options here.
+    // For more information, see https://aka.ms/blazor-standalone-auth
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+});
+
 builder.Services.AddTransient<ISpotifySongResolver, SpotifySongResolver>();
 
 builder.Services.AddTransient<IWorldReporter, WorldReporter>();
